8dcf8880ac79e4959b4bdfca87c586d951ebc89c
Aser-Essa <153296467+Aser-Essa@users.noreply.github.com>
2026-03-08 02:10:28 +0200

    implemet GET all subjects route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
 import express from "express";
+import cors from "cors";
+import subjectRouter from "./routes/subjects";
 
 const app = express();
 const PORT = 8000;
 
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "DELETE", "PUT", "POST"],
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
+
+app.use("/api/subjects", subjectRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
